a0003ad88ddb0fce06df9a183816c979ed47ccd9
Myles Voigt <myles.voigt@gmail.com>
2019-05-06 18:11:23 -0500

    added Google_Maps_API

--- Google_Maps_API.py
+++ Google_Maps_API.py
@@ -1,3 +1,23 @@
 import requests
 from requests.exceptions import HTTPError
 
+map_url = 'https://maps.googleapis.com/maps/api/staticmap?'
+
+class Google_Map_API():
+
+    def get_map(self, center, key):
+        #setup parameters for the api query
+        #todo setup to handle different quantities of stops
+        parameters = {'center': 'center', 'key': {key}}
+
+        #request data from google maps
+        try:
+            response = requests.get(map_url, params=parameters)
+            #raise error is certain response codes are returned
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f'HTTP error: {http_err}')
+        except Exception as ex:
+            print(f'Other error: {ex}')
+        else:
+            return response
