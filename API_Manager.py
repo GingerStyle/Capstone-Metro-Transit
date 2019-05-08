bf9e4588c8ca9a4b335627584a87c6b84fbcfd7f
clara <10088152+claraj@users.noreply.github.com>
2019-05-08 14:31:10 -0500

    restructure GUI, API calls

--- API_Manager.py
+++ API_Manager.py
@@ -1,33 +1,41 @@
 import os
 import Metro_Transit_API
 import Google_Maps_API
-import UI
+# import UI
 
 #get api key from the os
 maps_key = os.environ.get('GoogleMapsKey')
 
 #instantiate instances of APIs
-metroTransit = Metro_Transit_API
-googleMaps = Google_Maps_API
+# metroTransit = Metro_Transit_API
+# googleMaps = Google_Maps_API
 
 #instantiate instance of UI
-ui = UI
+# ui = UI
 
 #method to take routes and return to main controller as dictionary
 def get_routes():
     #get routes
-    response = metroTransit.get_routes()
+    response = Metro_Transit_API.get_routes()
+    route_numbers = route_numbers_from_response(response)
+    return route_numbers
+
+
+def route_numbers_from_response(response):
     #list to hold route numbers
+    print(response)
     route_numbers = []
     for route in response:
         print (route)
         route_numbers.append(route['Route'])
     #fill combobox with route numbers
-    ui.fill_routes(route_numbers)
+    print(route_numbers)
+    return route_numbers
 
 
 '''#method to get the directions for the route
-def get_directions():
+def get_directions(route_number):
+    
 
 
 #method to get the stops for the route and selected direction
